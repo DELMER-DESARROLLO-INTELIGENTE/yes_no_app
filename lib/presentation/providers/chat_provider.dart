f8e4d027be5d1dba539ba9785f71a1f3a7825077
Efrain May <efrain_may_75@hotmail.com>
2023-05-26 15:59:41 -0600

    mapper

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_asnwer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
 
   List<Message> messageList = [
@@ -16,7 +18,19 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
+    if(text.endsWith('?')){
+      herReply();
+    }
+
     notifyListeners();
+    moveScrollToBotton();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
+
+    notifyListeners();  
     moveScrollToBotton();
   }
 
